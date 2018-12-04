9a2ac91173157558d76f0d810e8aed0d30bd73aa
MarkGasse <mark.gasse@student.hu.nl>
2018-12-04 17:20:46 +0100

    Update week3_4.py

--- week3/week3_4.py
+++ week3/week3_4.py
@@ -20,3 +20,51 @@ outfile = 'dict.txt'
 dictNaarFile(outfile, voorkomen(infile))
 
 
+class TrieNode:
+    def __init__(self, n = 0, data = None):
+        if data == None:
+            data = dict()
+        self.n = n
+        self.data = data
+
+    def __repr__(self):
+        return str([self.data, self.n])
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        self.end = ' '
+        self._end = '__end__' #///////////
+
+    def __repr__(self):
+        return str(self.root)
+
+    def insert(self, woord):
+        current = self.root
+        if woord not in current.data.keys():
+            current.data[woord] = TrieNode()
+        current = current.data[woord]
+        if self.end not in current.data.keys():
+            current.data[self.end] = TrieNode()
+        current.data[self.end].n += 1
+
+    def find(self, woord):
+        current = self.root
+        if woord in current.data.keys():
+            current = current.data[woord]
+        else:
+            return False
+        return self.end in current.data.keys()
+
+def readFile(file):
+    myTrie = Trie()
+    with open(file, 'r') as infile:
+        tekst = infile.read()
+        for woord in tekst.split():
+            myTrie.insert(woord)
+    return myTrie
+
+print(readFile(infile))
+
+
+
