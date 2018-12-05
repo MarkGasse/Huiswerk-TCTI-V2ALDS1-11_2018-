0fc4c446d3a815ded9230fce91bbf01c055f718c
MarkGasse <mark.gasse@student.hu.nl>
2018-12-05 12:51:55 +0100

    Update week2_5.py

--- week2/week2_5.py
+++ week2/week2_5.py
@@ -6,13 +6,16 @@ def swap(a,i,j):
 import random
 
 counter = 0
-def qsort(a,low=0,high=-1):
+def qsort(worst_case,a,low=0,high=-1):
     global counter
     counter += 1
     if high == -1:
         high = len(a) -1
     if low < high:
-        swap(a,low, random.randint(low, high))
+        if not worst_case:
+            swap(a,low, random.randint(low, high))
+        else:
+            swap(a, low, high)
         m = low
         for j in range(low+1,high+1):
             if a[j] < a[low]:
@@ -24,39 +27,19 @@ def qsort(a,low=0,high=-1):
                     #  low <= i < m : a[i] < a[m]
                     #  i > m        : a[i] >= a[m]
                 if m > 0:
-                    qsort(a,low,m-1)
-                qsort(a,m+1,high)
+                    qsort(worst_case,a,low,m-1)
+                qsort(worst_case,a,m+1,high)
 
-def worst_case_qsort(a,low=0,high=-1):
-    global counter
-    counter += 1
-    if high == -1:
-        high = len(a) -1
-    if low < high:
-        swap(a,low,high)
-        m = low
-        for j in range(low+1,high+1):
-            if a[j] < a[low]:
-                m += 1
-                swap(a,m,j)
-                    # low < i <= m : a[i] < a[low]
-                    #  i > m        : a[i] >= a[low]
-                swap(a,low,m)
-                    #  low <= i < m : a[i] < a[m]
-                    #  i > m        : a[i] >= a[m]
-                if m > 0:
-                    qsort(a,low,m-1)
-                qsort(a,m+1,high)
 
 
 list = []
 random.seed(None)
 for i in range(10000):
     list.append(random.randint(1,10000))
-
-qsort(list)
+list2 = list
+qsort(False,list)
 print(counter)
 
 counter = 0
-worst_case_qsort(list)
+qsort(True,list)
 print(counter)
